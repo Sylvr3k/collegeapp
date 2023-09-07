5e9998911deee3a110e09cd8129a9b266d11f686
Sylvester <sangoshadrackkamenya@gmail.com>
2023-09-07 19:17:31 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,53 @@
+
+//this is where javascript code goes to present itself  
+        //passing information to the main page
+          /*Start
+                alert("Hello World");
+       
+                alert("I'm Sylvester");
+
+                alert("I just came to say hello");
+
+                alert("Hello---hello---hello-oo-o-o");
+
+                alert("Hello from the other side");
+
+                alert("Everytime I ask you, you say you are going to the party");
+                //passing information to console
+        
+                console.log("hello world for the console");
+
+                console.log("You know who it is!");
+
+                console.log("Black And Brown, Black and Brown, Black and Brown Yeah");
+
+                console.log("Pizza Angel plz come to me");
+            End*/    
+        /* Changing text from an html element*/
+        //document.getElementById("text").innerHTML = "This is the text from javascript";       
+            /*Start
+                function changeText() {
+                        document.getElementById("text").innerHTML = "This is the text from javascript";    
+                }
+            End*/
+        //Calling the function
+        /*
+        changeText();
+        */
+
+        function onePlus() {
+                document.getElementById('oneplusone').innerHTML = 1+1;
+        }
+
+        function twoPlus() {
+                document.getElementById('twoplustwo').innerHTML = 2+2;
+        }
+
+        function threePlus() {
+                document.getElementById('threeplusthree').innerHTML = 3+3;
+        }
+
+        function fourPlus() {
+                document.getElementById('fourplusfour').innerHTML = 4+4;
+        }
+
